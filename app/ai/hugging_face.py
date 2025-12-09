08a4749fb50fd9c43c642b32f70d988cae0d8693
MHDabdulkader <abdulkader201025@gmail.com>
2025-12-09 09:38:52 +0600

    today

--- app/ai/hugging_face.py
+++ app/ai/hugging_face.py
@@ -4,11 +4,17 @@ from app.config.settings import settings
 from fastapi import HTTPException
 from app.service.imagekit_service import upload_image_to_imagekit
 hf_token = settings.HUGGING_FACE_TOKEN
+from huggingface_hub import InferenceClient
+
+client = InferenceClient(
+    provider="nebius",
+    api_key=settings.HUGGING_FACE_TOKEN,
+)
 
 def generate_huggingface_image(prompt: str, style: str|None = None):
     full_prompt = f"{prompt},{style}" if style else prompt
     
-    API_URL = "https://api-inference.huggingface.co/models/CompVis/stable-diffusion-v1-4"
+    API_URL = "https://router.huggingface.co/models/CompVis/stable-diffusion-v1-4"
     headers = {"Authorization":f"Bearer {hf_token}"}
     
     
@@ -27,3 +33,11 @@ def generate_huggingface_image(prompt: str, style: str|None = None):
     imagekit_url = upload_image_to_imagekit(image_bytes, filename)
     print(f"======= image kit hugging face =============== ", imagekit_url)
     return imagekit_url
+
+def black_forest_labs_FLUX_1_dev(prompt: str, style: str|None = None):
+    full_prompt = f"{prompt},{style}" if style else prompt
+    response = client.text_to_image(
+        full_prompt,
+        model="black-forest-labs/FLUX.1-dev"
+    )
+    
--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,8 @@
 uvicorn app.main:app --host 0.0.0.0 --port 8000
+pip install --upgrade huggingface_hub
+
+pip install "pwdlib[argon2]"
+
+pip install pyjwt
+
+pip install passlib
